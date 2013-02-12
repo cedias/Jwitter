e68c66ec87e4f912ef3a6c46643f2eac13e0104f
Charles-Emmanuel <Charles-Emmanuel.Dias@etu.upmc.fr>
2013-02-12 08:23:21 +0100

    - Deleted MessageId() -> Useless

--- Dias_Ghanem/src/bd/Message/MessageTools.java
+++ Dias_Ghanem/src/bd/Message/MessageTools.java
@@ -22,20 +22,4 @@ public class MessageTools {
 	public static JSONObject listMessages(String username, int nb, int off) {
 		return null;
 	}
-	
-	public static JSONObject MessageId(String msg) {
-		JSONObject res = new JSONObject();
-		String id = "";
-		for(int i = 0 ; i < 6 ; i++){
-			Random r = new Random();
-			id = id + (char)(48 + r.nextInt(10));
-		}
-		try {
-			res.put("MessageId", id);
-			res.put("Message", msg);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return res;
-	}
 }
