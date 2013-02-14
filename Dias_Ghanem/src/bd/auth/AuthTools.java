22951f2be73ea831c18bdb69399ece76d37974de
Charles-Emmanuel Dias <Charles-Emmanuel.dias@etu.upmc.fr>
2013-02-14 15:09:44 +0100

    - Deleted useless methods

--- Dias_Ghanem/src/bd/auth/AuthTools.java
+++ Dias_Ghanem/src/bd/auth/AuthTools.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
-import org.json.JSONException;
+import javax.crypto.KeyGenerator;
+
 import org.json.JSONObject;
 
 import bd.Database;
@@ -99,7 +100,7 @@ public class AuthTools {
 	
 	public static JSONObject login(int username, String password)throws wrongPasswordException {
 		//check credentials
-		//creates new session
+		//creates new session (use generatekey)
 		//returns session as JSONObject {id,login,key}
 		return null;
 	}
@@ -134,58 +135,21 @@ public class AuthTools {
 			throw new SQLException(e.getMessage());
 		}
 	}
-	
-
-	public static boolean passwordValid(String userName,String password){
-		return true;
-	}
-	
-	public static boolean keyUsed(){
-		return true;
-	}
 
 	public static void logout() throws KeyInvalidException {
-		//removes it from the table of active keys in the bd
+		//put the field expired of the key to true
 		return;
 	}
-
-	public static boolean deactivate() {
-		//removes user
-		return true;
-	}
 	
-	
-	/*
-	 * creation d'un cle et il retour dans un format de string
-	 * il va le sauvgarde dans le db apres
-	 * 
-	 */
-	public static JSONObject createKey(String username) {
+	//totally unsecure Crap
+	private static String generateKey(){
+		String chars = "azertyuiopqsdfgERThjklmwwxcvbYUIOPQSDFGHJKLMWXCVBNn147825369AZ";
 		String key = "";
-		for(int i = 0 ; i <32 ; i++){
-			key = key + randomChar();
-		}
-		// sauvgarde
-		JSONObject json = new JSONObject();
-		try {
-			json.put("key", key);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return json;
-	}	
-	
-	private static char randomChar(){
-		
 		Random r = new Random();
-		double rnd = Math.random();
-		if(rnd < 0.3){
-			return (char) (97 + r.nextInt(25));
-		}else if(rnd < 0.7){
-			return (char)(48 + r.nextInt(10));
-		}else{
-			return (char)(65 + r.nextInt(25));
+		for(int i = 0; i<32; i++){
+			key+=r.nextInt(chars.length());
 		}
+		return key;
 	}
 
 	
