eeb8bc76ae0dae2d32cad3552ac56570574c0cb0
Charles-Emmanuel <Charles-Emmanuel.Dias@etu.upmc.fr>
2013-04-27 13:01:58 +0300

    Update BDStatic.java

--- Dias_Ghanem/src/bd/BDStatic.java
+++ Dias_Ghanem/src/bd/BDStatic.java
@@ -10,13 +10,13 @@ public interface BDStatic {
 
 	//mysql
 	public static boolean mysql_pooling = false;
-	public static String mysql_host = "li328.lip6.fr:33306";
-	public static String mysql_db   ="dias_ghanem";
-	public static String mysql_username = "dias_ghanem";
-	public static String mysql_password = "midg3tisfun";
+	public static String mysql_host = "";
+	public static String mysql_db   ="";
+	public static String mysql_username = "";
+	public static String mysql_password = "";
 	
 	//mongodb
-	public static String mongoDb_host = "li328.lip6.fr";
+	public static String mongoDb_host = "";
 	public static int mongoDb_port = 27130;
 	
 }
